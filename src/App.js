db03f1f614585f58b9f2448e9367763ac2324302
Sávio Mendes <saviomd@gmail.com>
2018-07-17 17:11:53 -0300

    Add component methods tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
 const Test = () => <div>Testing</div>;
 
@@ -21,6 +21,10 @@ class App extends Component {
       mainColor: 'blue',
       on: false,
     }
+  }
+  handleStrings(str) {
+    if (str === 'Hello World') return true;
+    return false;
   }
   componentDidMount() {
     this.setState({ lifeCycle: 'componentDidMount' });
